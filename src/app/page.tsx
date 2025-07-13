76408b674f1aab86595de1679afcb38aa6950484
Łukasz Nox <lukasznox@MacBook-Pro-ukasz.local>
2025-07-13 21:42:05 +0200

    repair input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useRef, type RefObject } from "react";
+import { useState, useRef, } from "react";
 
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
