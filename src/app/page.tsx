bbf5b73e9e53a43db544880f2ce8acf3e4fdd25a
Łukasz Nox <lukasznox@MacBook-Pro-ukasz.local>
2025-07-14 18:21:58 +0200

    fixed issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect useRef, } from "react";
+import { useState, useEffect, useRef, } from "react";
 
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
